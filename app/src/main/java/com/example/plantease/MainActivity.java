c254c913b48b583b637b9f09c261650cd556ddd1
Mudassir Bashir <mudassir786708@gmail.com>
2023-09-04 14:00:56 +0500

    Finished Project2

--- app/src/main/java/com/example/plantease/MainActivity.java
+++ app/src/main/java/com/example/plantease/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.plantease;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String VALID_USERNAME = "admin";
+    final String VALID_PASSWORD = "123456";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText usernameEdit = findViewById(R.id.userEdit);
+        EditText passwordEdit = findViewById(R.id.passwordEdit);
+        Button signinButton = findViewById(R.id.signinbtn);
+
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+
+                if (isValidCredentials(username, password)) {
+                    // Credentials are valid, navigate to the next activity
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish(); // Optional: Close the login activity to prevent going back
+                } else {
+                    // Invalid credentials, show a toast message
+                    Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    public boolean isValidCredentials(String username, String password) {
+        return username.equals(VALID_USERNAME) && password.equals(VALID_PASSWORD);
     }
 }
